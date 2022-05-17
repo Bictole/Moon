9d6911c2c7382867caafebdecbcde19045246b0a
Victor Simonin <victor.simonin@epita.fr>
2022-05-18 00:55:22 +0200

    [Add] test images, end section

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,37 @@ height: 100vh;
 function Home() {
   return (
     <Container>
-        <Section />
+        <Section 
+            title="MOON"
+            description="Beyond The Cloud"
+            backgroundImg="model-s.jpg"
+            leftBtnText="Lorem Ipsum"
+            rightBtnText="Lorem Ipsum"
+        />
+
+        <Section 
+            title="MOON 2"
+            description="Beyond The Cloud"
+            backgroundImg="model-y.jpg"
+            leftBtnText="Lorem Ipsum"
+            rightBtnText="Lorem Ipsum"
+        />
+
+        <Section 
+            title="MOON 3"
+            description="Beyond The Cloud"
+            backgroundImg="model-3.jpg"
+            leftBtnText="Lorem Ipsum"
+            rightBtnText="Lorem Ipsum"
+        />
+
+        <Section 
+            title="MOON 4"
+            description="Beyond The Cloud"
+            backgroundImg="model-x.jpg"
+            leftBtnText="Lorem Ipsum"
+            rightBtnText="Lorem Ipsum"
+        />
     </Container>
   )
 }
