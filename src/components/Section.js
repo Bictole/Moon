37cdff40bf6cdc6fa3f22a56a5c6f2adfa56dc46
Victor Simonin <victor.simonin@epita.fr>
2022-05-17 23:55:11 +0200

    [Add] Home and section components

---
+++ src/components/Section.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrap = styled.div`
+
+`
+
+function Section() {
+  return (
+    <Wrap>
+        Section
+    </Wrap>
+  )
+}
+
+export default Section
