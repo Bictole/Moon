4a97e82f218db461b2f79c49dd880e18b9a30e54
Victor Simonin <victor.simonin@epita.fr>
2022-05-18 01:55:42 +0200

    [Add] Header beginning

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,75 @@
 import React from 'react'
+import styled from 'styled-components'
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+const Container = styled.div`
+min-height: 60px;
+position: fixed;
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 0 20px;
+top: 0;
+left: 0;
+right: 0;
+`
+
+const Menu = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+flex: 1;
+
+a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+}
+
+@media (max-width: 768px) {
+    display: none;
+}
+`
+
+const RightMenu = styled.div`
+display: flex;
+align-items: center;
+
+a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+}
+`
+
+const CustomMenu = styled(MenuIcon)`
+cursor: pointer;
+`
+
 
 function Header() {
   return (
-    <div>
-        header
-    </div>
+    <Container>
+        <a>
+            <img src='/images/logo.svg' alt='/'/>
+        </a>
+
+        <Menu>
+            <a href='#'>Facility</a>
+            <a href='#'>Energy</a>
+            <a href='#'>Timeline</a>
+            <a href='#'>Money</a>
+
+        </Menu>
+
+        <RightMenu>
+            <a href='#'>Team</a>
+            <a href='#'>Hackathon</a>
+            <CustomMenu />
+        </RightMenu>
+    </Container>
   )
 }
 
