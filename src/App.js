5c41985957e87c27024e93519ab467cbb27823c5
Victor Simonin <victor.simonin@epita.fr>
2022-05-17 23:38:40 +0200

    [Add] Start React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
