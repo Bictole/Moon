0354de76b8ff212e18bfa4f9941148398e862a95
Victor Simonin <victor.simonin@epita.fr>
2022-09-03 23:41:12 +0200

    [Add] Timeline page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Main from "./components/Main"
 import Energy from "./components/Energy"
+import Timeline from './components/Timeline'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Main />} />
         <Route exact path='/energy' element={<Energy />} />
+        <Route exact path='/timeline' element={<Timeline />} />
       </Routes>
     </Router>
     
---
+++ src/components/Timeline.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Page from './Page'
+
+function Timeline() {
+  return (
+    <Page 
+            title="Timeline"
+            description="The timeline of the DataCenter launch"
+            backgroundImg=""
+            leftBtnText="Lorem Ipsum"
+    />
+  )
+}
+
+export default Timeline
